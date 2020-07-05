8742bf91f9cc903ebb72d9f1483ed87a08ed19d2
GitHub Action <action@github.com>
2020-07-05 15:21:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/ibobko/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ibobko/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ibobko.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ibobko.ideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ibobko/ideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ibobko/ideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ibobko.ideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ibobko.ideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
